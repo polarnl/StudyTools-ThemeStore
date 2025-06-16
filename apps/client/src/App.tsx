66b040612cd0179ccf8d89bb31ed043e53fc0e3f
Siem <vankeulensiem@gmail.com>
2025-06-16 15:21:46 +0200

    static store zonder styling

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,23 +1,45 @@
 function App() {
 
-  function inject() {
-    const json = `{
-  "name": "TEST2",
-  "author": "test"
-}`;
+  function inject(id: number) {
+    const json = store.themes.find(theme => theme.id === id);
     const el = document.createElement('StudyToolLoadComand');
-    el.setAttribute('themeJson', json);
+    el.setAttribute('themeJson', JSON.stringify(json));
     document.body.appendChild(el);
+  }
+  const store = {
+    version: "1.0",
+    themes: [
+      {
+        id: 1,
+        STVersion: 3.13,
+        name: "Magister",
+        description: "Het is magister, wat wil je nog meer weten?",
+        author: "Magister"
+      },
+      {
+        id: 2,
+        STVersion: 3.13,
+        name: "Magister Dark",
+        description: "Het is magister, maar dan in het donker.",
+        author: "Siemvk",
+        ptheme: "dark,207,95,2",
+        pagecolor: "false,0,0,58",
+        "sidecolor": "true,207,95,0"
+      }
+    ]
   }
 
   return (
     <>
       <div style={{ display: 'none' }} id="magisterStudyToolsInstalledVisible">
-        <h1>Hier komt de theme store, maar die moet ik nog maken...</h1>
-        <h2>Voor nu is hier een importknop</h2>
-        <button style={{ margin: '8px' }} onClick={inject}>
-          Import Theme
-        </button>
+        {store.themes.map(theme => (
+          <div key={theme.id}>
+            <h2>{theme.name}</h2>
+            <p>{theme.description}</p>
+            <p>Author: {theme.author}</p>
+            <button onClick={() => inject(theme.id)}>Install</button>
+          </div>
+        ))}
       </div>
       <div id="magisterStudyToolsInstalledNotVisible">
         <h1>Instaleer study tools voor magister om de store te kunnen gebruiken</h1>
